f15d906819f145be19cac844911f3d78a4265429
Litoxam <litobernier@gmail.com>
2026-07-27 14:04:49 +0200

    doc: added some docstrings

--- ai_service/agent.py
+++ ai_service/agent.py
@@ -1,9 +1,8 @@
 from google.adk.agents import Agent
 
+# Agent initialized without instruction. TO BUILD
 inventory_agent = Agent(
     name="inventory_agent",
     model="ollama_chat/qwen3:latest",
-    instruction="""
-    You are an inventory assistant.
-    """
+    instruction=""
 )
