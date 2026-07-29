637d67013658d695bd1d0df96fa60495a2c43c32
Litoxam <litobernier@gmail.com>
2026-07-29 14:16:03 +0200

    build: changed the docker compose and added the ports for the mcp_server

--- mcp_server/server.py
+++ mcp_server/server.py
@@ -1,6 +1,8 @@
-from mcp.server.fastmcp import FastMCP
+from fastmcp import FastMCP
 
 mcp = FastMCP("Product MCP Server")
 
 if __name__ == "__main__":
-    mcp.run()
+    mcp.run( transport="streamable-http",
+    host="0.0.0.0",
+    port=6000)
