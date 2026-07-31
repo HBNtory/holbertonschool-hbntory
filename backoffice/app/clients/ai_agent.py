4a33c2377005ef50f12ba263f950fd0d6584d1ee
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-07-31 05:59:34 +0200

    build: move ai_gent.py file from services to clients

--- backoffice/app/clients/ai_agent.py
+++ backoffice/app/clients/ai_agent.py
@@ -8,6 +8,10 @@ def send_to_ai_agent(query: str) -> dict:
     """send the user query body to the ai agent"""
     response = requests.post(
         f"{AGENT_URL}/query",
-        json={"query": query}
+        json={"query": query},
+        # Check if we need to put a timeout params here because if
+        # an agent is nok we block the entire process
+        # timeout = 30
     )
+    response.raise_for_status()
     return response.json()
